932a4ee1a2545d0c0dca82b93c08e042aad224cc
Ahmed Dewidar <a.mo.dewidar@gmail.com>
2020-03-07 04:38:23 +0200

    added express server with static ejs files

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const ejs = require('ejs')
+
+
+const app = express()
+app.set('view engine', 'ejs')
+app.use(express.static('static_files'))
+
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/new', (req, res) => {
+    res.render('new')
+})
+
+
+// Starting the server with port 3000
+app.listen(3000, () => {
+    console.log("Listening on port 3000")
+})
