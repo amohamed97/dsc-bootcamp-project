c3eb5eae7279f380a3b3d57e2553736d16a98a7f
Ahmed Dewidar <a.mo.dewidar@gmail.com>
2020-03-07 06:12:53 +0200

    can open an article and css link fix

--- app.js
+++ app.js
@@ -4,22 +4,26 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.set("view engine", "ejs");
-app.use(express.static("static_files"));
+app.use(express.static(__dirname + '/static_files'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Data
 let articlesArr = [
   {
+    id: 1,
     title: "1st Article",
     body:
       "Amet sit do tempor laborum. Qui in aliquip officia reprehenderit voluptate occaecat elit amet id elit ea. Sint sunt quis duis id eiusmod non sint aute."
   },
   {
+    id: 2,
     title: "2nd Article",
     body:
       "Amet sit do tempor laborum. Qui in aliquip officia reprehenderit voluptate occaecat elit amet id elit ea. Sint sunt quis duis id eiusmod non sint aute."
   }
 ];
+
+let nextID = 3
 
 // Routes
 app.get("/", (req, res) => {
@@ -33,11 +37,25 @@ app.get("/new", (req, res) => {
 app.post("/new", (req, res) => {
   submittedArticle = req.body;
   articlesArr.push({
+    id: nextID,
     title: submittedArticle.title,
     body: submittedArticle.body
   });
-  res.redirect('/')
+  nextID++;
+  res.redirect('/');
 });
+
+app.get('/articles/:articleId', (req, res) => {
+    let articleId = req.params.articleId;
+    let foundArticle = articlesArr.find(function(article){
+        return article.id === Number(articleId)
+    })
+    if(foundArticle){
+        res.render('article', {article: foundArticle})
+    }else{
+        res.send('Error')
+    }
+})
 
 // Starting the server with port 3000
 app.listen(3000, () => {
